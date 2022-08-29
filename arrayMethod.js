fa99168cc7d9a2f0497dcc8df834be6e444a05c4
Waad-Awajneh <waad.awajneh707@gmail.com>
2022-08-29 13:40:01 +0300

    ADD Q6

--- arrayMethod.js
+++ arrayMethod.js
@@ -114,3 +114,51 @@ return arr
 
 console.log(reversedString(['w','a','a','d']))
 
+/***********************************************************************************************************************/
+// Q5: regex
+// Write a function named isNum that takes in a string or number of any length. 
+// This function should use a regular expression pattern to return true if the input contains a number,
+//  and false if the input does not contain a number.
+
+// For example:
+// 12345 returns true
+// '12345' returns true
+// 'h3llo world' returns true
+// 'hello world' returns false
+
+
+function isNum(value){
+    
+let regex=/\w*\d+\w*/ig
+return regex.test( value)
+}
+
+console.log(isNum(12345))
+console.log(isNum('12345'))
+console.log(isNum('h3llo world'))
+console.log(isNum('hello worl5d'))
+console.log(isNum('hello world'))
+
+
+
+
+
+/********************************************************************************************************************** */
+
+// Q6:regex
+// You have created a game application and begin by asking users an easy question: In which month is Halloween?
+
+// Write a function named matchMonth which uses a regular expression pattern to match any of these 
+// inputs: October, Oct, october, oct
+
+// If the user enters any of these four inputs, return true. For any other input, return false.
+
+
+function matchMonth(text){
+  let regex=/October|Oct|october|oct/g;
+  if( text.match(regex)) return true;
+  return false;
+}
+
+let isMatch=matchMonth(prompt("In which month is Halloween?"));
+console.log(isMatch);
